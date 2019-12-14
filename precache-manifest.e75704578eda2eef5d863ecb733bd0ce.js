f3d29f1cac21cf60aee4f03f14d7af81da134bbb
sbahat1214 <bcs.f15.43@gmail.com>
2019-12-15 01:37:26 +0500

    f

---
+++ precache-manifest.e75704578eda2eef5d863ecb733bd0ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49db319900c24eb4d91cd058e2c0e40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "695c2b8c769db861c345",
+    "url": "/static/css/2.8d392223.chunk.css"
+  },
+  {
+    "revision": "dfce351cba3af26ac6f5",
+    "url": "/static/css/main.7d16470b.chunk.css"
+  },
+  {
+    "revision": "695c2b8c769db861c345",
+    "url": "/static/js/2.fdc13df5.chunk.js"
+  },
+  {
+    "revision": "23d9e510862350b3cfee56db67afcdac",
+    "url": "/static/js/2.fdc13df5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfce351cba3af26ac6f5",
+    "url": "/static/js/main.a271a8f4.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
